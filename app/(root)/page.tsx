9689752505bc4f059f0446fbd2a9f35b233a97f2
NessWrites <np01ai4s230009@islingtoncollege.edu.np>
2025-02-08 09:07:37 +0545

    Comment out unused values

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -4,7 +4,7 @@ import HeaderBox from '@/components/HeaderBox'
 import TotalBalanceBox from '@/components/TotalBalanceBox';
 import RightSidebar from '@/components/ui/RightSidebar';
 import React from 'react'
-import Image from "next/image";
+//import Image from "next/image";
 
 const Home = () => {
 	const loggedIn = {firstName: 'Ness', lastName:'Stha',
